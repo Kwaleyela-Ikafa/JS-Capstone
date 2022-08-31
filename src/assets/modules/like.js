092434549e52381033e9d8acbd310caa37ee1a8a
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-08-31 09:38:29 +0200

    add like function

---
+++ src/assets/modules/like.js
@@ -0,0 +1,61 @@
+const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/hmFVQXamfCrbM7Ii7gre/likes/';
+
+const getLikes = async () => {
+  const response = await fetch(baseUrl);
+  const likes = await response.json();
+  return likes;
+};
+
+const setLikes = async (id) => {
+  const response = await fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      item_id: id,
+    }),
+  }).then((data) => data.text())
+    .then((result) => result)
+    .catch(() => 'error');
+  return response;
+};
+
+// function to display number of likes
+
+const displayLikes = async () => {
+  const likesData = await getLikes();
+  const totalLikes = document.querySelectorAll('.likes-count');
+  totalLikes.forEach((likes) => {
+    const id = likes.getAttribute('data');
+    likesData.forEach((item) => {
+      if (item.item_id === id) likes.innerHTML = item.likes;
+    });
+  });
+};
+
+const addLikes = () => {
+  const likeBtn = document.querySelectorAll('.likes-btn');
+  likeBtn.forEach((heart) => {
+    heart.addEventListener('click', (e) => {
+      e.preventDefault();
+      const id = heart.getAttribute('id');
+      const currentLikes = parseInt(heart.innerText, 10);
+      setLikes(id).then((res) => {
+        if (res !== 'error') {
+          if (!res.error) {
+            const likesCount = document.querySelectorAll('.likes-count');
+            likesCount.forEach((likes) => {
+              if (e.target.id === id) {
+                likes.innerHTML = (currentLikes + 1);
+              }
+            });
+          }
+        }
+        displayLikes();
+      });
+    });
+  });
+};
+
+export { addLikes, displayLikes };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
-import _ from 'lodash';
 import './style.css';
+import { addLikes, displayLikes } from './assets/modules/like.js';
 
+const characterContainer = document.getElementById('characters');
+const apiUrl = 'https://bobsburgers-api.herokuapp.com/characters/';
+
+const characterData = [];
+
+const characterInformation = async () => {
+  const request = new Request(apiUrl);
+  const response = await fetch(request)
+    .then((res) => res.json)
+    .then((data) => data.result)
+    .catch(() => 'error');
+  return response;
+};
+
+// function to display character images
+
+const displayImg = () => {
+  fetch('https://bobsburgers-api.herokuapp.com/characters/')
+    .then((res) => res.json())
+    .then((data) => {
+      data = data.slice(0, 6);
+
+      if (data) {
+        data.forEach((character) => {
+          characterContainer.innerHTML += `
+      <li id="${character.id}" class="cards">
+        <div class="album-image">
+          <img src="${character.image}" alt="album ${character.name}">
+        </div>
+
+        <div class="like-section">
+          <p class="char-name"> ${character.name}</p>
+          <p id='${character.id}' class="likes-btn">
+          <i class="far fa-heart like-icon"> </i> <span class= "likes-count" data="${character.id}"></span><i> <strong>Likes </strong></i>
+          </p>
+        </div>
+        <button value="${character.id}" class="comment-btn" id="${character.id}">Comments</button>
+      </li>
+      `;
+          characterData.push(character);
+        });
+      }
+      addLikes();
+      displayLikes();
+    });
+};
+
+// function to count number of elements
+
+// const countItems = (items) => {
+//   let characters = 0;
+//   items.forEach((item) => {
+//     if (item.Type === ) {
+//       characters += 1;
+//     }
+//   });
+//   const characterCount = document.querySelectorAll('.character-count');
+//   characterCount.forEach((counter) => {
+//     counter.innerHTML = characters;
+//   });
+//   return characterCount;
+// };
+
+window.onload = () => {
+  displayImg();
+};
